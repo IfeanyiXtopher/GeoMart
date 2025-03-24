557f61ed719f7e40a2f755ef37f33591e3f16492
IfeanyiXtopher <nwagbaraxtopher@gmail.com>
2025-03-24 20:51:48 +0100

    changed location

--- js/custom.js
+++ js/custom.js
@@ -91,7 +91,7 @@ var owl = $('.portfolio_carousel').owlCarousel({
 
 // Initialize Map
 function initMap() {
-    const defaultLocation = { lat: 40.7128, lng: -74.0060 };
+    const defaultLocation = { lat: 5.016389, lng: 6.298889 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       center: defaultLocation,
@@ -175,7 +175,7 @@ $('.owl-filter-bar').on('click', '.item', function (e) {
 /** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(5.016389, 6.298889),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
